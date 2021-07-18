eb0ae4a64faa89b22a6264af6a69e7ff962d5778
andry9995 <andriramananarivo@gmail.com>
2021-07-18 19:58:09 +0300

    tables pour caisse

---
+++ src/AppBundle/Entity/Approvisionnement.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Approvisionnement
+ *
+ * @ORM\Table(name="approvisionnement")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ApprovisionnementRepository")
+ */
+class Approvisionnement
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime", nullable=true)
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="qte", type="decimal", precision=10, scale=2, nullable=true)
+     */
+    private $qte;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prix_achat", type="decimal", precision=10, scale=2, nullable=true)
+     */
+    private $prixAchat;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="total", type="decimal", precision=10, scale=2, nullable=true)
+     */
+    private $total;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @var \AppBundle\Entity\Produit
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Produit")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="produit", referencedColumnName="id")
+     * })
+     */
+    private $produit;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Approvisionnement
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set qte
+     *
+     * @param string $qte
+     *
+     * @return Approvisionnement
+     */
+    public function setQte($qte)
+    {
+        $this->qte = $qte;
+    
+        return $this;
+    }
+
+    /**
+     * Get qte
+     *
+     * @return string
+     */
+    public function getQte()
+    {
+        return $this->qte;
+    }
+
+    /**
+     * Set prixAchat
+     *
+     * @param string $prixAchat
+     *
+     * @return Approvisionnement
+     */
+    public function setPrixAchat($prixAchat)
+    {
+        $this->prixAchat = $prixAchat;
+    
+        return $this;
+    }
+
+    /**
+     * Get prixAchat
+     *
+     * @return string
+     */
+    public function getPrixAchat()
+    {
+        return $this->prixAchat;
+    }
+
+    /**
+     * Set total
+     *
+     * @param string $total
+     *
+     * @return Approvisionnement
+     */
+    public function setTotal($total)
+    {
+        $this->total = $total;
+    
+        return $this;
+    }
+
+    /**
+     * Get total
+     *
+     * @return string
+     */
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Approvisionnement
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set produit
+     *
+     * @param \AppBundle\Entity\Produit $produit
+     *
+     * @return Approvisionnement
+     */
+    public function setProduit(\AppBundle\Entity\Produit $produit = null)
+    {
+        $this->produit = $produit;
+    
+        return $this;
+    }
+
+    /**
+     * Get produit
+     *
+     * @return \AppBundle\Entity\Produit
+     */
+    public function getProduit()
+    {
+        return $this->produit;
+    }
+}
---
+++ src/AppBundle/Entity/Pannier.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Pannier
+ *
+ * @ORM\Table(name="pannier")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PannierRepository")
+ */
+class Pannier
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="pu", type="decimal", precision=10, scale=2, nullable=true)
+     */
+    private $pu;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="qte", type="decimal", precision=10, scale=2, nullable=true)
+     */
+    private $qte;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="total", type="decimal", precision=10, scale=2, nullable=true)
+     */
+    private $total;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime", nullable=true)
+     */
+    private $date;
+
+    /**
+     * @var \AppBundle\Entity\Produit
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Produit")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="produit", referencedColumnName="id")
+     * })
+     */
+    private $produit;
+
+    /**
+     * @var \AppBundle\Entity\Commande
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Commande")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="commande", referencedColumnName="id")
+     * })
+     */
+    private $commande;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set pu
+     *
+     * @param string $pu
+     *
+     * @return Pannier
+     */
+    public function setPu($pu)
+    {
+        $this->pu = $pu;
+    
+        return $this;
+    }
+
+    /**
+     * Get pu
+     *
+     * @return string
+     */
+    public function getPu()
+    {
+        return $this->pu;
+    }
+
+    /**
+     * Set qte
+     *
+     * @param string $qte
+     *
+     * @return Pannier
+     */
+    public function setQte($qte)
+    {
+        $this->qte = $qte;
+    
+        return $this;
+    }
+
+    /**
+     * Get qte
+     *
+     * @return string
+     */
+    public function getQte()
+    {
+        return $this->qte;
+    }
+
+    /**
+     * Set total
+     *
+     * @param string $total
+     *
+     * @return Pannier
+     */
+    public function setTotal($total)
+    {
+        $this->total = $total;
+    
+        return $this;
+    }
+
+    /**
+     * Get total
+     *
+     * @return string
+     */
+    public function getTotal()
+    {
+        return $this->total;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Pannier
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set produit
+     *
+     * @param \AppBundle\Entity\Produit $produit
+     *
+     * @return Pannier
+     */
+    public function setProduit(\AppBundle\Entity\Produit $produit = null)
+    {
+        $this->produit = $produit;
+    
+        return $this;
+    }
+
+    /**
+     * Get produit
+     *
+     * @return \AppBundle\Entity\Produit
+     */
+    public function getProduit()
+    {
+        return $this->produit;
+    }
+
+    /**
+     * Set commande
+     *
+     * @param \AppBundle\Entity\Commande $commande
+     *
+     * @return Pannier
+     */
+    public function setCommande(\AppBundle\Entity\Commande $commande = null)
+    {
+        $this->commande = $commande;
+    
+        return $this;
+    }
+
+    /**
+     * Get commande
+     *
+     * @return \AppBundle\Entity\Commande
+     */
+    public function getCommande()
+    {
+        return $this->commande;
+    }
+}
