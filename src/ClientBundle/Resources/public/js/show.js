33d8de8fc6c774fffdcfe564f09e7f921a2e757c
andry9995 <andriramananarivo@gmail.com>
2021-09-20 07:29:51 +0300

    bouton maj client

--- src/ClientBundle/Resources/public/js/show.js
+++ src/ClientBundle/Resources/public/js/show.js
@@ -20,7 +20,8 @@ $(document).ready(function(){
 			let clm_email = $('input[name=clm_email]').val();
 			let clm_tel_contact = $('input[name=clm_tel_contact]').val();
 			if (clm_nom_societe != '' && clm_tel_fixe != '' && clm_tel_contact != '') {
-				form.submit();
+				var data = form.serializeArray();
+				save_client(data, form.data('action'));
 			} else {
 				show_info('Erreur','Champs obligatoires','error');
 			}
@@ -31,7 +32,8 @@ $(document).ready(function(){
 			let clp_tel = $('input[name=clp_tel]').val();
 			let clp_sexe = $('select[name=clp_sexe]').val();
 			if (clp_nom != '' && clp_adresse != '' && clp_tel != '' && clp_sexe != '') {
-				form.submit();
+				var data = form.serializeArray();
+				save_client(data, form.data('action'));
 			} else {
 				show_info('Erreur','Champs obligatoires','error');
 			}
@@ -39,5 +41,17 @@ $(document).ready(function(){
 
 
 
-	})
+	});
+
+	function save_client(data,url) {
+		$.ajax({
+			url: url,
+			type: 'POST',
+			data: data,
+			success: function(res) {
+				show_info('Succés','Client enregistré');
+				location.reload();
+			}
+		})
+	}
 });
